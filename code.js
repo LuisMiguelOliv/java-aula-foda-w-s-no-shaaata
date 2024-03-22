917101345fbcd9801756606e58d25b9c37cbbbd2
LuisMiguelOliv <161032102+LuisMiguelOliv@users.noreply.github.com>
2024-03-22 11:08:08 -0300

    Hey there, I'm using WhatsApp!

---
+++ code.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const listaNomes = document.body.querySelector ('#listaNomes');
+    const inputNome = document.body.querySelector ('#inputNome');
+    const buttonadicionar = document.body.querySelector ('#buttonadicionar');
+
+    buttonadicionar.addEventListener('click', () => {
+        const div = document.createElement('div');
+        div.innerHTML = '<li>' + inputNome.value + '</li>';
+        listaNomes.appendChild(div.firstElementChild);
+    });
+});
